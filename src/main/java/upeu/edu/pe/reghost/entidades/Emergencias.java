88503dd42ac72a75e7b0548195c173e484606f9d
3PM-J <jos3pm123@gmail.com>
2025-06-29 15:41:59 -0500

    Simplified Emergency Registration System

--- src/main/java/upeu/edu/pe/reghost/entidades/Emergencias.java
+++ src/main/java/upeu/edu/pe/reghost/entidades/Emergencias.java
@@ -9,82 +9,30 @@ public class Emergencias {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 60)
-    private String nombrePaciente;
+    @Column(nullable = false, length = 100)
+    private String nombreCompleto;
 
     @Column(nullable = false, length = 60)
-    private String apellidoPaciente;
-
-    @Column(nullable = false, length = 15)
-    private String dniPaciente;
-
-    @Column(nullable = false)
-    private LocalDateTime fechaHoraIngreso;
-
-    @Column(nullable = false, length = 20)
-    private String tipoEmergencia;
-
-    @Column(nullable = false, length = 20)
-    private String nivelPrioridad;
-
-    @Column(nullable = false, length = 500)
-    private String descripcionCaso;
-
-    @Column(nullable = false, length = 200)
-    private String sintomasPrincipales;
-
-    @Column(nullable = false, length = 15)
-    private String telefonoContacto;
-
-    @Column(nullable = false, length = 60)
-    private String contactoEmergencia;
-
-    @Column(nullable = false, length = 15)
-    private String telefonoEmergencia;
-
-    @Column(length = 60)
     private String doctorAsignado;
 
-    @Column(nullable = false, length = 20)
-    private String estado;
+    @Column(nullable = false)
+    private LocalDateTime fechaHora;
 
-    @Column(length = 100)
-    private String ambulancia;
+    @Column(nullable = false, length = 30)
+    private String estadoSalud;
 
-    @Column(length = 200)
-    private String hospital;
-
-    @Column(length = 500)
+    @Column(length = 300)
     private String observaciones;
 
-    @Column(length = 200)
-    private String tratamientoInicial;
-
     // Constructor completo
-    public Emergencias(Long id, String nombrePaciente, String apellidoPaciente, String dniPaciente,
-                       LocalDateTime fechaHoraIngreso, String tipoEmergencia, String nivelPrioridad,
-                       String descripcionCaso, String sintomasPrincipales, String telefonoContacto,
-                       String contactoEmergencia, String telefonoEmergencia, String doctorAsignado,
-                       String estado, String ambulancia, String hospital, String observaciones,
-                       String tratamientoInicial) {
+    public Emergencias(Long id, String nombreCompleto, String doctorAsignado, 
+                       LocalDateTime fechaHora, String estadoSalud, String observaciones) {
         this.id = id;
-        this.nombrePaciente = nombrePaciente;
-        this.apellidoPaciente = apellidoPaciente;
-        this.dniPaciente = dniPaciente;
-        this.fechaHoraIngreso = fechaHoraIngreso;
-        this.tipoEmergencia = tipoEmergencia;
-        this.nivelPrioridad = nivelPrioridad;
-        this.descripcionCaso = descripcionCaso;
-        this.sintomasPrincipales = sintomasPrincipales;
-        this.telefonoContacto = telefonoContacto;
-        this.contactoEmergencia = contactoEmergencia;
-        this.telefonoEmergencia = telefonoEmergencia;
+        this.nombreCompleto = nombreCompleto;
         this.doctorAsignado = doctorAsignado;
-        this.estado = estado;
-        this.ambulancia = ambulancia;
-        this.hospital = hospital;
+        this.fechaHora = fechaHora;
+        this.estadoSalud = estadoSalud;
         this.observaciones = observaciones;
-        this.tratamientoInicial = tratamientoInicial;
     }
 
     // Constructor vacío
@@ -101,92 +49,12 @@ public class Emergencias {
         this.id = id;
     }
 
-    public String getNombrePaciente() {
-        return nombrePaciente;
+    public String getNombreCompleto() {
+        return nombreCompleto;
     }
 
-    public void setNombrePaciente(String nombrePaciente) {
-        this.nombrePaciente = nombrePaciente;
-    }
-
-    public String getApellidoPaciente() {
-        return apellidoPaciente;
-    }
-
-    public void setApellidoPaciente(String apellidoPaciente) {
-        this.apellidoPaciente = apellidoPaciente;
-    }
-
-    public String getDniPaciente() {
-        return dniPaciente;
-    }
-
-    public void setDniPaciente(String dniPaciente) {
-        this.dniPaciente = dniPaciente;
-    }
-
-    public LocalDateTime getFechaHoraIngreso() {
-        return fechaHoraIngreso;
-    }
-
-    public void setFechaHoraIngreso(LocalDateTime fechaHoraIngreso) {
-        this.fechaHoraIngreso = fechaHoraIngreso;
-    }
-
-    public String getTipoEmergencia() {
-        return tipoEmergencia;
-    }
-
-    public void setTipoEmergencia(String tipoEmergencia) {
-        this.tipoEmergencia = tipoEmergencia;
-    }
-
-    public String getNivelPrioridad() {
-        return nivelPrioridad;
-    }
-
-    public void setNivelPrioridad(String nivelPrioridad) {
-        this.nivelPrioridad = nivelPrioridad;
-    }
-
-    public String getDescripcionCaso() {
-        return descripcionCaso;
-    }
-
-    public void setDescripcionCaso(String descripcionCaso) {
-        this.descripcionCaso = descripcionCaso;
-    }
-
-    public String getSintomasPrincipales() {
-        return sintomasPrincipales;
-    }
-
-    public void setSintomasPrincipales(String sintomasPrincipales) {
-        this.sintomasPrincipales = sintomasPrincipales;
-    }
-
-    public String getTelefonoContacto() {
-        return telefonoContacto;
-    }
-
-    public void setTelefonoContacto(String telefonoContacto) {
-        this.telefonoContacto = telefonoContacto;
-    }
-
-    public String getContactoEmergencia() {
-        return contactoEmergencia;
-    }
-
-    public void setContactoEmergencia(String contactoEmergencia) {
-        this.contactoEmergencia = contactoEmergencia;
-    }
-
-    public String getTelefonoEmergencia() {
-        return telefonoEmergencia;
-    }
-
-    public void setTelefonoEmergencia(String telefonoEmergencia) {
-        this.telefonoEmergencia = telefonoEmergencia;
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
     }
 
     public String getDoctorAsignado() {
@@ -197,28 +65,20 @@ public class Emergencias {
         this.doctorAsignado = doctorAsignado;
     }
 
-    public String getEstado() {
-        return estado;
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
     }
 
-    public String getAmbulancia() {
-        return ambulancia;
+    public String getEstadoSalud() {
+        return estadoSalud;
     }
 
-    public void setAmbulancia(String ambulancia) {
-        this.ambulancia = ambulancia;
-    }
-
-    public String getHospital() {
-        return hospital;
-    }
-
-    public void setHospital(String hospital) {
-        this.hospital = hospital;
+    public void setEstadoSalud(String estadoSalud) {
+        this.estadoSalud = estadoSalud;
     }
 
     public String getObservaciones() {
@@ -227,13 +87,5 @@ public class Emergencias {
 
     public void setObservaciones(String observaciones) {
         this.observaciones = observaciones;
-    }
-
-    public String getTratamientoInicial() {
-        return tratamientoInicial;
-    }
-
-    public void setTratamientoInicial(String tratamientoInicial) {
-        this.tratamientoInicial = tratamientoInicial;
     }
 }
