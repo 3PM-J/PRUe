58083fa2264cc4e0a1292e6825efb93256e6cd39
3PM-J <jos3pm123@gmail.com>
2025-06-29 21:44:27 -0500

    Generate and Download Word Documents for Patients

--- src/main/java/upeu/edu/pe/reghost/controlador/PacienteControlador.java
+++ src/main/java/upeu/edu/pe/reghost/controlador/PacienteControlador.java
@@ -1,12 +1,18 @@
 package upeu.edu.pe.reghost.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import upeu.edu.pe.reghost.entidades.Pacientes;
 import upeu.edu.pe.reghost.servicio.PacienteServicio;
+import upeu.edu.pe.reghost.servicio.DocumentoWordServicio;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -15,6 +21,9 @@ public class PacienteControlador {
 
     @Autowired
     private PacienteServicio pacienteServicio;
+    
+    @Autowired
+    private DocumentoWordServicio documentoWordServicio;
 
     @GetMapping("/")
     public String listarPacientes(Model modelo) {
@@ -47,6 +56,26 @@ public class PacienteControlador {
     public String eliminarPaciente(@PathVariable("id") Long id) {
         pacienteServicio.delete(id);
         return "redirect:/pacientes/";
+    }
+
+    @GetMapping("/descargar-word/{id}")
+    public ResponseEntity<byte[]> descargarDocumentoWord(@PathVariable("id") Long id) {
+        try {
+            Pacientes paciente = pacienteServicio.get(id);
+            byte[] documentoBytes = documentoWordServicio.generarDocumentoPaciente(paciente);
+            
+            String nombreArchivo = "Ficha_Medica_" + paciente.getNombre() + "_" + paciente.getApellido() + ".docx";
+            
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", nombreArchivo);
+            headers.setContentLength(documentoBytes.length);
+            
+            return new ResponseEntity<>(documentoBytes, headers, HttpStatus.OK);
+            
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // Mantener compatibilidad con rutas anteriores
